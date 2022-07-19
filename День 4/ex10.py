8cd561f446bbce3576a9c3980e030f32ecb5790b
Ahmetova <70811430+Ahmetova@users.noreply.github.com>
2022-07-19 19:28:03 +0300

    Update ex10.py

--- День 4/ex10.py
+++ День 4/ex10.py
@@ -4,14 +4,11 @@ def function(data):
     newlist = []
     j = 0
     for i in range(0, len(data)):
-        #rint('len',len(data), 'j=',j)
         if (data[j] % 2) == 0:
             data[j] = round(data[j]/2)
             j += 1
-            #rint(data)
         else:
             del data[j]
-            #rint(data)
     mylist = data
 
 print('Введие список чисел через пробел:')
